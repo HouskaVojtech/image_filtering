e889ae5836125377d44322455041252e86e7ddfb
crivoi-andrei <crivoi@healper.dk>
2022-12-18 10:08:24 +0100

    updated code and stuff

--- assignment_3/scripts/beethoven.py
+++ assignment_3/scripts/beethoven.py
@@ -1,55 +1,89 @@
 import ps_utils
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import pickle
+
+from enum import Enum
 
 
-def main(file_name='Beethoven'):
-    # read Beethoven data
-    I, mask, S = ps_utils.read_data_file(file_name)
+class Datasets(Enum):
+    BEETHOVEN = 'Beethoven'
+    MAT = 'mat_vase'
+    SHINY = 'shiny_vase'
+    SHINY2 = 'shiny_vase2'
+    BUDDHA = 'Buddha'
+    FACE = 'face'
 
-    # get indices of non zero pixels in mask
-    nz = np.argwhere(mask > 0)
-    m, n = mask.shape
 
-    flat_mask = mask.flatten()
-    Is = []
-    for i in range(3):
-        Is.append(I[:, :, i].flatten())
+def main(ransac=False, savefig=False, unbiased_integrate=True):
+    dataset = Datasets.FACE.value
+    path = f'src/{dataset}_{"ransac" if ransac else "woodham"}'
 
-    stacked_I = np.vstack(Is)
+    I, mask, S = ps_utils.read_data_file(dataset)
+    count_Is = I.shape[2]
 
-    J = stacked_I[:, flat_mask > 0]
+    nz = np.nonzero(mask)
+    count_nz = len(nz[0])
 
-    M = np.dot(np.linalg.inv(S), J)
+    J = np.vstack([I[:, :, i][nz] for i in range(count_Is)])
+    assert (J.shape == (count_Is, count_nz))
 
-    # The part bellow is not mine
+    if ransac:
+        ransac_threshold = 10.
+        ransac_data = (J, S)
+        M, inliers, fit = ps_utils.ransac_3dvector(ransac_data, ransac_threshold)
+        pickle.dump((M, inliers, fit), open(f'src/ransac_runs/ransac_3dvector_{dataset}.sav', 'wb'))
+    else:
+        try:
+            M = np.linalg.inv(S) @ J
+        except np.linalg.LinAlgError:
+            M = np.linalg.pinv(S) @ J
 
-    # get albedo as norm of M and normalize M
     Rho = np.linalg.norm(M, axis=0)
-    N = M / np.tile(Rho, (3, 1))
+    N = M / np.tile(Rho, (M.shape[0], 1))
 
-    n1 = np.zeros((m, n))
-    n2 = np.zeros((m, n))
-    n3 = np.ones((m, n))
+    albedo = np.zeros(mask.shape)
+    albedo[nz] = Rho
+    plt.title(f'Albedo {dataset}')
+    plt.imshow(albedo)
+    plt.axis('off')
+    if savefig:
+        plt.savefig(f'{path}_albedo.png')
+    plt.show()
+
+    n1 = np.zeros(mask.shape)
+    n2 = np.zeros(mask.shape)
+    n3 = np.zeros(mask.shape)
 
     n1[nz] = N[0, :]
     n2[nz] = N[1, :]
     n3[nz] = N[2, :]
 
-    _, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    _, (ax1, ax2, ax3) = plt.subplots(1, M.shape[0])
     ax1.imshow(n1)
+    ax1.set_title(r'N1')
     ax1.axis('off')
     ax2.imshow(n2)
+    ax2.set_title(r'N2')
     ax2.axis('off')
     ax3.imshow(n3)
+    ax3.set_title(r'N3')
     ax3.axis('off')
     plt.tight_layout()
+    plt.suptitle(f'Normals {dataset}')
+    if savefig:
+        plt.savefig(f'{path}_normals.png')
     plt.show()
 
-    z = ps_utils.unbiased_integrate(n1, n2, n3, mask)
-    ps_utils.display_surface_matplotlib(z)
+    if unbiased_integrate:
+        z = ps_utils.unbiased_integrate(n1, n2, n3, mask)
+    else:
+        z = ps_utils.simchony_integrate(n1, n2, n3, mask)
+    # z = ps_utils.simchony_integrate(n1, n2, n3, mask)
+    ps_utils.display_surface(z)
 
 
 if __name__ == '__main__':
-    main()
-    # main('mat_vase')
+    main(ransac=True,
+         savefig=True,
+         unbiased_integrate=True)
